329a26af434f91c27c64b7bdec9177f0c896f51c
phakphumi <pakpoom.t@student.chula.ac.th>
2018-12-29 13:02:37 +0700

    connect to airtable

--- personal-helper.js
+++ personal-helper.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import Webtask from 'webtask-tools';
 import bodyParser from 'body-parser';
+import Airtable from 'airtable';
 import {
   middleware,
   Client,
@@ -9,6 +10,7 @@ import {
 import {
   get,
   head,
+  split,
 } from 'lodash/fp';
 
 const app = express();
@@ -21,9 +23,32 @@ app.post('/webhook', (req, res) => {
   const event = head(get(['body', 'events'])(req));
   const callingMessage = get(['message', 'text'])(event);
   const { replyToken } = event;
+
+  addSpendingRecord({ secrets: get(['webtaskContext', 'secrets'])(req) })
+
   client.replyMessage(replyToken, toLineMessage(callingMessage));
   res.sendStatus(200);
 });
+
+const transformIncomingMessage = (message) => {
+  split(' ')(message);
+};
+
+const addSpendingRecord = ({ secrets: { AIRTABLE_API_KEY, AIRTABLE_PERSONAL_HELPER_BASE } }) => {
+  const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_PERSONAL_HELPER_BASE);
+  base('SpendingRecords').create({
+    "category": "food",
+    "amount": 100,
+    "recordType": "expense",
+    "userId": "3"
+  }, (err, record) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(record.getId());
+  });
+};
 
 const toLineMessage = (message) => {
   if(!message) { message = '...'; }
