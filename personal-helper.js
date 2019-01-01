013de5b6c7dc50d0fd9337c4d98ba38af4a3c750
phakphumi <pakpoom.t@student.chula.ac.th>
2019-01-01 17:53:40 +0700

    record expense to airtable

--- personal-helper.js
+++ personal-helper.js
@@ -3,6 +3,7 @@ import express from 'express';
 import Webtask from 'webtask-tools';
 import bodyParser from 'body-parser';
 import Airtable from 'airtable';
+import moment from 'moment-timezone';
 import {
   middleware,
   Client,
@@ -10,7 +11,7 @@ import {
 import {
   get,
   head,
-  split,
+  isObject,
 } from 'lodash/fp';
 
 const app = express();
@@ -22,35 +23,74 @@ app.post('/webhook', (req, res) => {
   const client = new Client(lineConfig);
   const event = head(get(['body', 'events'])(req));
   const callingMessage = get(['message', 'text'])(event);
+  const secrets = get(['webtaskContext', 'secrets'])(req);
   const { replyToken } = event;
 
-  addSpendingRecord({ secrets: get(['webtaskContext', 'secrets'])(req) })
-
-  client.replyMessage(replyToken, toLineMessage(callingMessage));
+  try {
+    addSpendingRecord({
+      userId: get(['source', 'userId'])(event),
+      secrets,
+      callback: ({ amount, category }) => {
+        client.replyMessage(replyToken, toLineMessage({ message: `Recorded expense ${amount} baht on ${category}`}));
+      },
+      ...transformIncomingMessage({ message: callingMessage }),
+    });
+  } catch (error) {
+    console.log(error);
+    if(error.errorDesc) {
+      client.replyMessage(replyToken, toLineMessage({ message: error.errorDesc }));
+    }
+    return error;
+  }
   res.sendStatus(200);
 });
 
-const transformIncomingMessage = (message) => {
-  split(' ')(message);
+const transformIncomingMessage = ({ message }) => {
+  const matchedMessage = message.match(/^([\d.]+)([tfghmol])$/i);
+  if(!matchedMessage) {
+    console.log('Incoming message has incorrect format');
+    throw {
+      errorCode: 1,
+      errorDesc: `Incorrect format for message: \"${message}\" while transforming.\nPlease sending message with this format \"[Number][Category]\".\neg. 100f`
+    };
+  }
+  let transformedMessage = {};
+  transformedMessage.category = {
+    d: 'drink',
+    f: 'food',
+    h: 'health',
+    m: 'miscellaneous',
+    o: 'occasion',
+    s: 'snack',
+    t: 'transportation',
+  }[matchedMessage[2].toLowerCase()];
+  transformedMessage.amount = parseFloat(matchedMessage[1]);
+  return transformedMessage;
 };
 
-const addSpendingRecord = ({ secrets: { AIRTABLE_API_KEY, AIRTABLE_PERSONAL_HELPER_BASE } }) => {
+const addSpendingRecord = ({
+  secrets: { AIRTABLE_API_KEY, AIRTABLE_PERSONAL_HELPER_BASE },
+  userId,
+  category,
+  amount,
+  callback,
+}) => {
   const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_PERSONAL_HELPER_BASE);
   base('SpendingRecords').create({
-    "category": "food",
-    "amount": 100,
-    "recordType": "expense",
-    "userId": "3"
+    category,
+    amount,
+    userId,
+    createdDate: moment.tz('Asia/Bangkok').format('YYYY-MM-DD'),
   }, (err, record) => {
     if (err) {
       console.error(err);
       return;
     }
-    console.log(record.getId());
+    callback({ amount: get(['fields', 'amount'])(record), category: get(['fields', 'category'])(record) });
   });
 };
 
-const toLineMessage = (message) => {
+const toLineMessage = ({ message }) => {
   if(!message) { message = '...'; }
   return [{
     type: 'text',
